1458ea3520f26eeef36be535cab861cd597ce9a0
David McKee <git-author.n4t20@simplelogin.com>
2025-11-12 22:36:41 -0500

    updated install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,13 @@ gh_username=daevski
 repo_name=kitbash
 working_dir="$HOME/Downloads"
 
-git clone https://github.com/$gh_username/$repo_name $working_dir
+git clone https://github.com/$gh_username/$repo_name $working_dir/$repo_name
 chmod +x $working_dir/$repo_name/kit-start.sh
 echo "alias kit=$working_dir/$repo_name/kit-start.sh" >> $HOME/.bashrc
-source $HOME/.bashrc
-echo "Alias 'kit' created been created for the kitbash core script: kit-start.sh; example 'kit hostname mypc'"
+
+echo "Setup complete! Alias 'kit' added to ~/.bashrc"
+echo ""
+echo "To use the alias now, run:"
+echo "  source ~/.bashrc"
+echo ""
+echo "Or simply open a new terminal window."
